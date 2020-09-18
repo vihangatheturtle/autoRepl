6d9f270a81e3f70dd5e08d9b7986ebfb727c53ec
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2020-09-18 21:09:12 +0100

    Update start.bat

---
+++ start.bat
@@ -0,0 +1,9 @@
+@echo off
+
+echo Starting 5 tasks
+start /min python "C:\Users\Vihanga1432\Documents\PythonProjects\autoRepl\main.py"
+start /min python "C:\Users\Vihanga1432\Documents\PythonProjects\autoRepl\main.py"
+start /min python "C:\Users\Vihanga1432\Documents\PythonProjects\autoRepl\main.py"
+start /min python "C:\Users\Vihanga1432\Documents\PythonProjects\autoRepl\main.py"
+start /min python "C:\Users\Vihanga1432\Documents\PythonProjects\autoRepl\main.py"
+echo Started
